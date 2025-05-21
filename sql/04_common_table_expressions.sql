af0ee994ca80acb2e90ef48a48960a9426e4061c
VibeHarboe <61502171+steelduckii@users.noreply.github.com>
2025-05-21 08:33:06 +0200

    Update 04_common_table_expressions.sql

--- sql/04_common_table_expressions.sql
+++ sql/04_common_table_expressions.sql
@@ -1,6 +1,7 @@
 -- #########################################################
 -- Common Table Expressions (CTEs) for modular, readable,
 -- and reusable query components across transformations
+-- For more advanced CTE's, check out --> 05_window_functions.sql
 -- #########################################################
 
 -- ========================================================
@@ -160,6 +161,6 @@ LEFT JOIN away ON m.id = away.id
 WHERE season = '2014/2015'
   AND (home.team_long_name = 'Manchester United' OR away.team_long_name = 'Manchester United');
 
-## ================================================================================================================================
-## For more advanced CTE's, check out --> https://github.com/steelduckii/Data-Manipulation-in-SQL/blob/main/sql/05_window_functions.sql
-## ================================================================================================================================
+## ===============================================================
+## For more advanced CTE's, check out --> 05_window_functions.sql
+## ===============================================================
